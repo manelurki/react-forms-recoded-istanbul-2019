5aec1c5c1604fa684fdcffa65a5908d67208640e
manel turki <menelturki@gmail.com>
2020-03-24 15:57:41 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -20,10 +20,18 @@ class Form extends React.Component {
 
   render() {
     return (
-      <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
-      </form>
+          <form>
+              <input
+                type="text"
+                onChange={event => this.props.handleFirstNameChange(event)}
+                value={this.props.formData.firstName}
+              />
+              <input
+                type="text"
+                onChange={event => this.props.handleLastNameChange(event)}
+                value={this.props.formData.lastName}
+              />
+            </form>
     )
   }
 }
